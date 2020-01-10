1dce8e909923ae34e11a93ba27ac031a141c587d
Cody Toney-Griffith <cody.toney@gmail.com>
2020-01-10 02:08:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+def initialize (name)
+  @name = name 
+end
+
+def name
+  @name 
+end
+def initialize (breed)
+  @breed = breed
+end 
+def breed
+  @breed = breed
+end
+end
+puppy = Dog.new 
