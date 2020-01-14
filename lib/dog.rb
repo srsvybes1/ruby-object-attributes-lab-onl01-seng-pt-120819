ee01ddf0515652a70672407293d561336a5b355d
Cody Toney-Griffith <cody.toney@gmail.com>
2020-01-14 22:50:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,14 @@
 class Dog 
-def initialize (name)
-  @name = name 
-end
-
 def name
   @name 
 end
-def initialize (breed)
-  @breed = breed
-end 
+def name=(name) 
+@name=name 
+end
 def breed
+  @breed
+end 
+def breed=(breed)
   @breed = breed
 end
 end
-puppy = Dog.new 
